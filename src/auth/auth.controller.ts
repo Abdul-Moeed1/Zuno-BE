22b96aa813304c68925272655709e66f36bb8935
Abdul Moeed <abdulmoeedjhelumi@gmail.com>
2025-11-09 21:11:19 +0500

    Login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDTO } from "./DTOs/signUp.dto";
+import { LogInDTO } from "./DTOs/logIn.dto";
 
 @Controller("auth")
 export class AuthController {
     constructor(private readonly AuthService: AuthService) { }
-    @Post()
+    @Post("signup")
     async Signup(@Body() Body: SignUpDTO) {
         return this.AuthService.Signup(Body)
     }
 
+    @Post("login")
+    async login(@Body() body: LogInDTO) {
+        return this.AuthService.Login(body)
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,32 @@ import { SignUpDTO } from "./DTOs/signUp.dto";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { User, UserDocument } from "src/user/models/user.model";
+import { LogInDTO } from "./DTOs/logIn.dto";
+import JWT from "jsonwebtoken"
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) { }
+    private readonly JwtSecret: string;
+    constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>
+    ) { this.JwtSecret = process.env.JWT_SECRET_KEY as string }
     Signup = async (body: SignUpDTO) => {
-        const student = new this.UserModel(body)
-        const response = await student.save()
+        const user = new this.UserModel(body)
+        const response = await user.save()
         return response
     }
 
-    Login = async () => {
-        
+    Login = async (body: LogInDTO) => {
+        const { email, password } = body;
+        const user = await this.UserModel.findOne({ email });
+        if (user) {
+            if (user.password === password) {
+                const token = await JWT.sign({ id: user._id, email: email }, this.JwtSecret)
+                return token
+            } else {
+                return "Wrong Credentials"
+            }
+        } else {
+            return "No User Found"
+        }
     }
 }
