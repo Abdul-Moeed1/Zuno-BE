29ad737c417a49e22bca3c6c6f9d10f3a26fca23
Abdul Moeed <abdulmoeedjhelumi@gmail.com>
2025-12-07 21:59:44 +0500

    Cors origin working

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors({ origin: process.env.CLIENT_URL })
 
   const config = new DocumentBuilder()
     .setTitle("Zuno")
